2ed7d8d00e04161597d6d0ab6c07f408d7ac09f3
Camilo Lopez <johan.lopez@bluetab.net>
2020-08-12 21:03:24 -0500

    Obtener autor por id funcionando

--- server.js
+++ server.js
@@ -33,4 +33,31 @@ app.post('/autores', (req, res) => {
     }
 });
 
+/**
+ * Obtener un autor por id.
+ */
+app.get('/autores/:id', (req, res) => {
+    const { id } = req.params;
+
+    const author = getAuthorById(id);
+
+    if (author) {
+        res.status(200)
+            .json({ author });
+    } else {
+        res.status(404)
+            .json({ message: 'No existe un autor con id ' + id });
+    }
+});
+
+/**
+ * Obtener un autor por su Id.
+ * @param {Number} id Id del autor
+ * @return {Author} Objeto autor
+ */
+const getAuthorById = (id) => autores.find(autor => autor.id == id);
+
+/**
+ * El servidor esta escuchando en el puerto 3000.
+ */
 app.listen(3000, () => console.log('Servidor escuchando en puerto 3000'));
