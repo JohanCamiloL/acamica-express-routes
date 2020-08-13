2e2ddd2f635b42daae8764390b7840d08e48fde5
Camilo Lopez <johan.lopez@bluetab.net>
2020-08-13 10:51:46 -0500

    Book model created

---
+++ model/Book.js
@@ -0,0 +1,10 @@
+class Book {
+    constructor(id, titulo, descripcion, anioPublicacion) {
+        this.id = id;
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.anioPublicacion = anioPublicacion
+    }
+}
+
+module.exports = Book;
